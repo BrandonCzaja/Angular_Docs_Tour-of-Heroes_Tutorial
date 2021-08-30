03a002042debe6043b2c43011f1a868329077381
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-30 16:43:28 -0500

    Refactored the hero component template(html) to enable two-way data binding for the input. This allows the hero name to update as I type in the input field. Imported the FormsModule from @angular/forms. Finished the Hero Editor section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; //  NgModel lives here
+
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesComponent } from './components/heroes/heroes.component'; // Imported automatically by the Angular CLI
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
