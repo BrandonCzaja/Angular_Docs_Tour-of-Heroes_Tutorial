88fbdcd1bfd1c8220148e1f59065c3bf52c0a751
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-30 22:09:05 -0500

    Swapped the of() from rxjs for http.get()

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,19 +3,23 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { Observable, of } from 'rxjs'; // Observable imports
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 // Injectable decorator
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes'; // Url to web api
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
   // returns mock data
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   // Get an individual hero by id
@@ -23,6 +27,10 @@ export class HeroService {
     const hero = HEROES.find((h) => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id: ${id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
 
