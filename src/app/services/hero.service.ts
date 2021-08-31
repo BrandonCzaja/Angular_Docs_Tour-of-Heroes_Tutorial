40132523dcb0f6bdb1d911823d74621378540dd7
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-31 06:43:49 -0500

    Can now delete heroes. However I just realized that none of the changes persist because they aren't going to an actual database or server. Those functionalities are mock.

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -54,6 +54,16 @@ export class HeroService {
     );
   }
 
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   /*
     Handle Http operation that failed. Lets the app continue
     @param operation - name of the operation that failed
