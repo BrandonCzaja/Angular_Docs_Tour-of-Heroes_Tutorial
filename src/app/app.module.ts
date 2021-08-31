8aaedad672212b50a6d112b5fa12c9848891278c
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-30 21:46:20 -0500

    Imported the HttpClientModule to enable communicating with a remote server over HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'; //  NgModel lives here
+import { HttpClientModule } from '@angular/common/http'; // Step 1 of 2 in making the HttpClient available throughout the entire application. Step 2 of 2 is to add it to the imports array below
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -10,8 +11,14 @@ import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component'; // Imported automatically by the Angular CLI
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
