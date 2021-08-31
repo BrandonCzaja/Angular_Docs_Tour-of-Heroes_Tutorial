9a6745db855c0f3427918482630596d73dca54cf
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-30 20:28:53 -0500

    Added additonal messaging services so that the history of selected heroes is shown

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../../hero'; // Hero Interface
+
+// Services
 import { HeroService } from '../../services/hero.service';
+import { MessageService } from '../../services/message.service';
 
 // Decorator function that specifies the Angular metadata for the component.
 @Component({
@@ -9,17 +13,13 @@ import { HeroService } from '../../services/hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  // Heroes Component Property
-  heroes: Hero[] = [];
   selectedHero?: Hero;
+  heroes: Hero[] = []; // Heroes component property
 
-  // Hero Component property
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   // Lifecycle hook. Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic. The void{} means that it doesn't return anything.
   ngOnInit(): void {
@@ -28,6 +28,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // Subscribe is kind of like a promise. It is needed for when HTTP requests are made
